50a9f6427a22a5961edd12c9f33a0d8351d77afe
FedkinDmitriy <fedkindmitriy@yandex.ru>
2024-10-18 12:43:45 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LabWhile_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewMain = new System.Windows.Forms.ListView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.columnAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBoxUserString = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewMain
+            // 
+            this.listViewMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnAddress,
+            this.columnName,
+            this.columnDescription});
+            this.listViewMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewMain.GridLines = true;
+            this.listViewMain.HideSelection = false;
+            this.listViewMain.Location = new System.Drawing.Point(0, 0);
+            this.listViewMain.Name = "listViewMain";
+            this.listViewMain.Size = new System.Drawing.Size(466, 343);
+            this.listViewMain.TabIndex = 0;
+            this.listViewMain.UseCompatibleStateImageBehavior = false;
+            this.listViewMain.View = System.Windows.Forms.View.Details;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonCheck);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxUserString);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewMain);
+            this.splitContainer1.Size = new System.Drawing.Size(823, 343);
+            this.splitContainer1.SplitterDistance = 353;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // columnAddress
+            // 
+            this.columnAddress.Text = "Address";
+            this.columnAddress.Width = 57;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 125;
+            // 
+            // columnDescription
+            // 
+            this.columnDescription.Text = "Description";
+            this.columnDescription.Width = 278;
+            // 
+            // textBoxUserString
+            // 
+            this.textBoxUserString.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUserString.Location = new System.Drawing.Point(17, 39);
+            this.textBoxUserString.Multiline = true;
+            this.textBoxUserString.Name = "textBoxUserString";
+            this.textBoxUserString.Size = new System.Drawing.Size(316, 251);
+            this.textBoxUserString.TabIndex = 0;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(258, 296);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheck.TabIndex = 1;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 373);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewMain;
+        private System.Windows.Forms.ColumnHeader columnAddress;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnDescription;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.TextBox textBoxUserString;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabWhile_6
+{
+    public partial class Form1 : Form
+    {
+        readonly string patternVar = @"[a-zA-Z]+[0-9]+";
+        readonly string patternConst = @"(true)";
+        readonly string patternIsWhile = @"^while\s*\(\s*([a-zA-Z]+[0-9]+)\s*([\+\-\*\/])\s*([a-zA-Z]+[0-9]+)\s*:=\s*(true)\s*\)\s*\{((\s*([a-zA-Z]+[0-9]+)\s*([\+\-\*\/])\s*([a-zA-Z]+[0-9]+)\s*)*;)*\s*\}$";
+
+        private List<string> Tokens { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //textBoxUserString.Text = "while(as22+ee4:=true){as22+ee4;}";
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            string temp = textBoxUserString.Text.Trim();
+            
+            bool isWhile = Regex.IsMatch(temp, patternIsWhile);
+
+            if(isWhile)
+            {
+                textBoxUserString.BackColor = Color.LimeGreen;
+
+                MatchCollection matchesVar = Regex.Matches(temp, patternVar);
+                MatchCollection matchesConst = Regex.Matches(temp, patternConst);
+
+                Result(matchesVar, matchesConst);
+
+                listViewMain.Items.Clear();
+                
+                int addressCounter = 1; // Счётчик для адреса токенов
+
+                if (Tokens != null)
+                {
+                    foreach (var token in Tokens)
+                    {
+                        ListViewItem item = new ListViewItem(addressCounter.ToString()); // columnAddress
+                        item.SubItems.Add(token); // columnName
+                        if (addressCounter == 1)
+                        {
+                            item.SubItems.Add("константа символов"); // columnDescription
+                        }
+                        item.SubItems.Add("целочисленная переменная"); // columnDescription
+                        listViewMain.Items.Add(item);
+                        addressCounter++;
+                    }
+                }
+            }
+            else
+            {
+                textBoxUserString.BackColor = Color.Red;
+                listViewMain.Items.Clear();
+                MessageBox.Show("Uncorrect string","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+        private void Result(MatchCollection matchesVar, MatchCollection matchesConst)
+        {
+            List<string> result = new List<string>();
+
+            foreach (Match match in matchesConst)
+            {
+                result.Add(match.Value);
+            }
+            foreach (Match match in matchesVar)
+            {
+                result.Add(match.Value);
+            }
+            Tokens = result;
+        }
+
+
+    }
+}
