305ef831176d354b70ab1b052d79bff335c09e83
FedkinDmitriy <fedkindmitriy@yandex.ru>
2024-10-18 12:52:46 +0300

    правки: назвавние,кто сделал

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,13 @@
         private void InitializeComponent()
         {
             this.listViewMain = new System.Windows.Forms.ListView();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.columnAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.textBoxUserString = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.buttonCheck = new System.Windows.Forms.Button();
+            this.textBoxUserString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -52,27 +53,10 @@
             this.listViewMain.HideSelection = false;
             this.listViewMain.Location = new System.Drawing.Point(0, 0);
             this.listViewMain.Name = "listViewMain";
-            this.listViewMain.Size = new System.Drawing.Size(466, 343);
+            this.listViewMain.Size = new System.Drawing.Size(467, 343);
             this.listViewMain.TabIndex = 0;
             this.listViewMain.UseCompatibleStateImageBehavior = false;
             this.listViewMain.View = System.Windows.Forms.View.Details;
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.buttonCheck);
-            this.splitContainer1.Panel1.Controls.Add(this.textBoxUserString);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.listViewMain);
-            this.splitContainer1.Size = new System.Drawing.Size(823, 343);
-            this.splitContainer1.SplitterDistance = 353;
-            this.splitContainer1.TabIndex = 1;
             // 
             // columnAddress
             // 
@@ -89,14 +73,23 @@
             this.columnDescription.Text = "Description";
             this.columnDescription.Width = 278;
             // 
-            // textBoxUserString
+            // splitContainer1
             // 
-            this.textBoxUserString.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxUserString.Location = new System.Drawing.Point(17, 39);
-            this.textBoxUserString.Multiline = true;
-            this.textBoxUserString.Name = "textBoxUserString";
-            this.textBoxUserString.Size = new System.Drawing.Size(316, 251);
-            this.textBoxUserString.TabIndex = 0;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonCheck);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxUserString);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewMain);
+            this.splitContainer1.Size = new System.Drawing.Size(823, 343);
+            this.splitContainer1.SplitterDistance = 352;
+            this.splitContainer1.TabIndex = 1;
             // 
             // buttonCheck
             // 
@@ -108,6 +101,26 @@
             this.buttonCheck.UseVisualStyleBackColor = true;
             this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
             // 
+            // textBoxUserString
+            // 
+            this.textBoxUserString.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUserString.Location = new System.Drawing.Point(17, 59);
+            this.textBoxUserString.Multiline = true;
+            this.textBoxUserString.Name = "textBoxUserString";
+            this.textBoxUserString.Size = new System.Drawing.Size(316, 231);
+            this.textBoxUserString.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Лабораторная работа #6             \"Лексический анализ\"";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -115,7 +128,7 @@
             this.ClientSize = new System.Drawing.Size(851, 373);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Fedkin Dmitriy O-22-IVT-1";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -134,6 +147,7 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button buttonCheck;
         private System.Windows.Forms.TextBox textBoxUserString;
+        private System.Windows.Forms.Label label1;
     }
 }
 
